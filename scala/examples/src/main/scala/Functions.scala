94de1be1514c8afea01d006ab0be993f6b2e3dc4
Sébastian Le Merdy <567637+seblm@users.noreply.github.com>
2023-10-22 18:09:38 +0200

    :pencil2: Teacher presentation and function composition examples

--- scala/examples/src/main/scala/Functions.scala
+++ scala/examples/src/main/scala/Functions.scala
@@ -48,19 +48,28 @@ object Functions:
         discountByCategories
       )
     )
+    logBeforeAndAfter("hello", () => { println("Hello World!") })
+    logBeforeAndAfterResult("hello", message => message.length)
+    println(operationToCompute("add")(3, 2))
+    println(operationToCompute("subtract")(3, 2))
+    println(operationToCompute("multiply")(3, 2))
+    println(operationToCompute("divide")(3, 2))
+
 case class User(name: String, age: Int)
 enum Category:
   case Young, Adult, Old
-val categoryByMaxAges: Map[Int, Category] = Map(
-  24 -> Category.Young,
-  64 -> Category.Adult,
-  Integer.MAX_VALUE -> Category.Old
-)
-val discountByCategories: Map[Category, Int] = Map(
-  Category.Young -> 25,
-  Category.Adult -> 0,
-  Category.Old -> 35
-)
+val categoryByMaxAges: Map[Int, Category] =
+  Map(
+    24 -> Category.Young,
+    64 -> Category.Adult,
+    Integer.MAX_VALUE -> Category.Old
+  )
+val discountByCategories: Map[Category, Int] =
+  Map(
+    Category.Young -> 25,
+    Category.Adult -> 0,
+    Category.Old -> 35
+  )
 def computePrice(
     user: User,
     categories: Map[Int, Category],
@@ -97,3 +106,23 @@ def findCategory(
     .filter { case (maxAge, category) => maxAge >= user.age }
     .minBy { case (maxAge, category) => maxAge }
   category
+
+def logBeforeAndAfter(message: String, task: () => Unit): Unit =
+  println(s"before $message")
+  task()
+  println(s" after $message")
+
+def logBeforeAndAfterResult(
+    message: String,
+    computeResult: String => Long
+): Unit =
+  println(s"before $message")
+  println(s"result ${computeResult(message)}")
+  println(s" after $message")
+
+def operationToCompute(operation: String): (Long, Long) => Long =
+  operation match
+    case "add"      => (left, right) => left + right
+    case "subtract" => (left, right) => left - right
+    case "multiply" => (left, right) => left * right
+    case _          => (left, right) => Long.MinValue
